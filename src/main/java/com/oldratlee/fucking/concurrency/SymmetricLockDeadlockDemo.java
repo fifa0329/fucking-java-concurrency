9051e43f9384cb160b128f63dc42f74257560d32
lingxiang.wang <lingxiang.wang@autonavi.com>
2015-04-02 14:46:43 +0800

    死锁案例

--- src/main/java/com/oldratlee/fucking/concurrency/SymmetricLockDeadlockDemo.java
+++ src/main/java/com/oldratlee/fucking/concurrency/SymmetricLockDeadlockDemo.java
@@ -2,6 +2,20 @@ package com.oldratlee.fucking.concurrency;
 
 /**
  * @author Jerry Lee (oldratlee at gmail dot com)
+ *
+ *
+ *  对称锁死锁
+
+# 问题分析见文章链接：对称锁死锁，对应的英文文章：Synchronization on mutable fields
+Demo类com.oldratlee.fucking.concurrency.SymmetricLockDeadlockDemo。
+
+Demo说明
+
+主线程中开启2个任务线程执行。
+
+问题说明
+
+任务线程死锁。
  */
 public class SymmetricLockDeadlockDemo {
     static final Object lock1 = new Object();
@@ -12,6 +26,9 @@ public class SymmetricLockDeadlockDemo {
         thread1.start();
         Thread thread2 = new Thread(new ConcurrencyCheckTask2());
         thread2.start();
+
+
+        System.out.print("The End\n");
     }
 
     private static class ConcurrencyCheckTask1 implements Runnable {
@@ -21,6 +38,8 @@ public class SymmetricLockDeadlockDemo {
             while (true) {
                 synchronized (lock1) {
                     synchronized (lock2) {
+                        //有可能这个占据了两个lock，导致一直在hello
+                        //也有可能大家各自占据了一个lock，导致死锁
                         System.out.println("Hello1");
                     }
                 }
