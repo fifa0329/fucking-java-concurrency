2999e5da6bbe284986fecaf74ccc2497868b66e5
lingxiang.wang <lingxiang.wang@autonavi.com>
2015-04-02 15:24:22 +0800

    hashmap这种数据结构 在多线程的情况下 应该使用支持并发的

--- src/main/java/com/oldratlee/fucking/concurrency/HashMapHangDemo.java
+++ src/main/java/com/oldratlee/fucking/concurrency/HashMapHangDemo.java
@@ -5,6 +5,20 @@ import java.util.Map;
 import java.util.Random;
 
 /**
+ *
+ *
+ * :beer: HashMap的死循环
+
+ 这个问题在疫苗：Java HashMap的死循环等多个地方都有讲解。
+ Demo类com.oldratlee.fucking.concurrency.HashMapHangDemo，可以复现这个问题。
+
+ Demo说明
+
+ 主线程中开启2个任务线程执行HashMap的put操作。主线程做get操作。
+
+ 问题说明
+
+ 通过没有持续的输出判定主线程Block，即HashMap的出现死循环。
  * @author Jerry Lee (oldratlee at gmail dot com)
  * @see <a href="http://coolshell.cn/articles/9606.html">Java HashMap的死循环</a>@<a href="http://weibo.com/haoel">左耳朵耗子</a>
  */
@@ -28,6 +42,8 @@ public class HashMapHangDemo {
                 demo.holder.get(j);
 
                 // 如果出现hashmap的get hang住问题，则下面的输出就不会再出现了。
+//                http://coolshell.cn/articles/9606.html
+//                此demo说明了并发情况要用并发的数据结构
                 // 在我的开发机上，很容易在第一轮就观察到这个问题。
                 System.out.printf("Got key %s in round %s\n", j, i);
             }
